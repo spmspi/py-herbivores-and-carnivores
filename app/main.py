c544e55a06a73f8fc8cffff4e6011685f2febd03
SPMSPI <kislicin.artur@gmail.com>
2026-03-01 14:10:47 +0200

    fixed errors #Code Style.5

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 class Animal:
     alive: list["Animal"] = []
 
-    def __init__(self,
-                 name: str,
-                 health: int = 100,
-                 hidden: bool = False) -> None:
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False) -> None:
         self.name = name
         self._health = health
         self.hidden = hidden
@@ -36,6 +37,8 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    def bite(self, target: Animal) -> None:
+    def bite(
+            self,
+            target: Animal) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             target.health -= 50
