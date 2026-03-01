c2061be1ee455eddf0623244f5b783ac2489809d
SPMSPI <kislicin.artur@gmail.com>
2026-03-01 13:47:58 +0200

    ready

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self._health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    @property
+    def health(self) -> int:
+        return self._health
+
+    @health.setter
+    def health(self, value: int) -> None:
+        self._health = value
+        if self._health <= 0:
+            self._health = 0
+            if self in Animal.alive:
+                Animal.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self._health},"
+                f" Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+    def hide(self: Animal) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, target: Animal) -> Animal:
+        if isinstance(target, Herbivore) and not target.hidden:
+            target.health -= 50
